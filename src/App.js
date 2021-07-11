0fb0382d2740d19238d9a4934d07d1003495ee25
jun-yuaka <85206734+jun-yuaka@users.noreply.github.com>
2021-07-11 15:13:35 +0900

    organismの学習

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 
 import "./styles.css";
+
+const user = {
+  name: "jan",
+  image: "https://source.unsplash.com/brFsZ7qszSY",
+  email: "ppppp@example.com",
+  phone: "090-8888-9999",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
